2eaedb1d561d87dd4afc1d2ff2c6e1a08410892a
dzianis.one <dzianis.one@Dzianiss-MacBook-Pro.local>
2024-03-20 15:40:11 +0100

    env in nextCongig

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
 		NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
 		NEXT_PUBLIC_EMAILJS_KEY: process.env.NEXT_PUBLIC_EMAILJS_KEY,
 	},
+	env: {
+		NEXT_PUBLIC_GOOGLE_MAP_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY,
+		NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
+		NEXT_PUBLIC_EMAILJS_KEY: process.env.NEXT_PUBLIC_EMAILJS_KEY,
+	},
 	reactStrictMode: false,
 	swcMinify: true,
 	compiler: {
