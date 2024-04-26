bf8c7928d0949ec350ea0b7342cdf5d2f5841846
simonriple <simon.riple@gmail.com>
2024-04-26 16:26:04 +0200

    update ref

--- pages/index.tsx
+++ pages/index.tsx
@@ -353,7 +353,7 @@ const Home: NextPage<HomePageProps> = ({ theme, themeToggler, themeName }) => {
 						<Text>{t('heroDescription')}</Text>
 					</Parallax>
 					<Parallax disabled={isMobile} speed={-100} translateY={['0', '-50%']} opacity={[1, -1]} shouldAlwaysCompleteAnimation>
-						<ScrollDownStyled onClick={() => ourStoryRef?.current?.scrollIntoView({ behavior: 'smooth' })}>
+						<ScrollDownStyled onClick={() => aboutUsRef?.current?.scrollIntoView({ behavior: 'smooth' })}>
 							<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 								<path d="M7 0V12.175L1.4 6.575L0 8L8 16L16 8L14.6 6.575L9 12.175V0H7Z" />
 							</svg>
